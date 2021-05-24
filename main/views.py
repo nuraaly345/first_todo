1fb7181f74367f5d6b2db0c617f7bd24d956b25d
nuraaly345 <kubanura57@gmail.com>
2021-05-24 15:56:30 +0300

    added model and in admin

--- main/views.py
+++ main/views.py
@@ -1,11 +1,13 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render, HttpResponse
+from .models import ToDo
 
 def index(request):
     return render(request, 'index.html')
 
 
 def test(request):
+    
     return render(request, 'test.html')
 
 def check(request):
